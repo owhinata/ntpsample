ca2e6dc06e73a14628a1dd3ca3dbb7e19a1c0fba
KatsumiOuwa <d2p.yggdrasill@gmail.com>
2025-11-02 20:08:43 +0900

    build: shallow-fetch gtest; enable server example; ignore *.swp
    - cmake/FetchGTest.cmake: add GIT_SHALLOW TRUE to FetchContent_Declare
    - ntpserver/CMakeLists.txt: set NTP_SERVER_BUILD_EXAMPLE=ON by default
    - .gitignore: add *.swp and remove tracked swap file

--- cmake/FetchGTest.cmake
+++ cmake/FetchGTest.cmake
@@ -14,8 +14,8 @@ function(ntp_fetch_gtest)
     googletest
     GIT_REPOSITORY https://github.com/google/googletest.git
     GIT_TAG ${NTPG_TAG}
+    GIT_SHALLOW TRUE
   )
   set(INSTALL_GTEST OFF CACHE BOOL "" FORCE)
   FetchContent_MakeAvailable(googletest)
 endfunction()
-
